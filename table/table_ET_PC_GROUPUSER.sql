0bcedf3e94a04fc202d9f04e27fdf7d9917a1cee
SajimAkhtar <sajim.akhtar@kasmo.co>
2023-09-26 11:02:47 +0530

    Automated file commit

---
+++ table/table_ET_PC_GROUPUSER.sql
@@ -0,0 +1,15 @@
+create or replace TABLE ET_PC_GROUPUSER (
+	"LoadCommandID" NUMBER(38,0),
+	"PublicID" VARCHAR(16777216),
+	"TextField1" VARCHAR(16777216),
+	"TextField2" VARCHAR(16777216),
+	"TextField3" VARCHAR(16777216),
+	"UserID" NUMBER(38,0),
+	"BeanVersion" NUMBER(38,0),
+	"Manager" BOOLEAN,
+	"LoadFactorType" NUMBER(38,0),
+	"LoadFactor" NUMBER(38,0),
+	"Member" BOOLEAN,
+	"GroupID" NUMBER(38,0),
+	ID NUMBER(38,0)
+);
